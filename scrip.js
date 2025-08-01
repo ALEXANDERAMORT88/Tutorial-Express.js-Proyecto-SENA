6f53ed67953b077b56df393fcac5ce489729ad73
ALEXANDERAMORT88 <alex.88m.aa@gmail.com>
2025-08-01 15:38:55 -0500

    finalizando el tutorial, gracias

--- scrip.js
+++ scrip.js
@@ -1,7 +1,25 @@
+// Ejemplo de uso de JavaScript 
 const nombre = "Alexander";
 
 function saludar(nombre) {
     return `Hola, ${nombre}`;
-}
+};
 
 console.log(saludar(nombre));
+
+
+
+// Creando servidor con Express.js
+
+const express = require('express');
+const app = express();
+
+// Ruta de prueba 
+app.get('/', (req, res) => {
+    res.end('¡Hola mundo con Express');
+});
+
+// El servidor queda escuchando
+app.listen(3000, () => {
+    console.log("Serrvidor corriendo en http://localhost:3000");
+});
